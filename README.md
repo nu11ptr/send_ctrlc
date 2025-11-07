979d190308c8825f6d9f4c7bb775dc71e43a7ce0
Scott Meeuwsen <3615587+nu11ptr@users.noreply.github.com>
2025-11-06 19:39:08 -0500

    Add tokio feature

--- README.md
+++ README.md
@@ -6,3 +6,57 @@
 [![codecov](https://codecov.io/github/nu11ptr/send_ctrlc/graph/badge.svg?token=3M5tvBewE5)](https://codecov.io/github/nu11ptr/send_ctrlc)
 
 A cross platform crate for sending ctrl-c to child processes
+
+## Features
+
+* Cross platform (including Windows)
+* Uniform cross platform API
+* Both sync and async
+* Minimal dependencies: 
+    * Synchronous: `libc` on unix, and `windows-sys` on windows
+    * Asynchronous: `tokio` (with only `process` feature)
+
+## Examples
+
+```rust
+use send_ctrlc::{Interruptable as _};
+
+// Synchronous...
+
+#[cfg(not(feature = "tokio"))]
+fn main() {
+        // Start a continuous ping using our special command constructor function
+        let mut command = send_ctrlc::new_command("ping");
+        #[cfg(windows)]
+        command.arg("-t");
+        command.arg("127.0.0.1");
+
+        // Spawn the ping, interrupt it, and wait for it to complete
+        let mut child = command.spawn().unwrap();
+        child.send_ctrl_c().unwrap();
+        child.wait().unwrap();
+}
+
+// or asynchronous...
+
+#[cfg(feature = "tokio")]
+#[tokio::main]
+async fn main() {
+        // Start a continuous ping using our special command constructor function
+        let mut command = send_ctrlc::new_tokio_command("ping");
+        #[cfg(windows)]
+        command.arg("-t");
+        command.arg("127.0.0.1");
+
+        // Spawn the ping, interrupt it, and wait for it to complete
+        let mut child = command.spawn().unwrap();
+        child.send_ctrl_c().unwrap();
+        child.wait().await.unwrap();
+}
+
+```
+
+
+## Contributions
+
+Contributions are welcome as long they align with the vision for this crate.
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
+//! A cross platform crate for sending ctrl-c to child processes
+
+#[doc = include_str!("../README.md")]
+mod readme_tests {}
+
 use std::ffi::OsStr;
 use std::io;
 use std::process::{Child, Command};
@@ -7,31 +12,39 @@ const CREATE_NEW_PROCESS_GROUP: u32 = 0x00000200;
 
 /// Trait for sending interrupts/ctrl-c to child processes
 pub trait Interruptable: InterruptablePid {
+    /// Send a ctrl-c interrupt to the child process
     #[cfg(all(not(windows), not(unix)))]
     fn send_ctrl_c(&self) -> io::Result<()> {
         unimplemented!("Not implemented for this platform");
     }
 
+    /// Send a ctrl-c interrupt to the child process
     #[cfg(unix)]
     fn send_ctrl_c(&self) -> io::Result<()> {
-        if unsafe { libc::kill(self.pid() as i32, libc::SIGINT) } == 0 {
-            Ok(())
+        if let Some(pid) = self.pid() {
+            if unsafe { libc::kill(pid as i32, libc::SIGINT) } == 0 {
+                Ok(())
+            } else {
+                Err(io::Error::last_os_error())
+            }
         } else {
-            Err(io::Error::last_os_error())
+            Err(io::Error::new(io::ErrorKind::Other, "Process has no pid"))
         }
     }
 
+    /// Send a ctrl-c interrupt to the child process
     #[cfg(windows)]
     fn send_ctrl_c(&self) -> io::Result<()> {
         use windows_sys::Win32::System::Console::{CTRL_C_EVENT, GenerateConsoleCtrlEvent};
-
-        unsafe {
+        if let Some(pid) = self.pid() {
             // NOTE: This only works if the process is in a new process group
-            if GenerateConsoleCtrlEvent(CTRL_C_EVENT, self.pid()) == 0 {
-                Err(io::Error::last_os_error())
-            } else {
+            if unsafe { GenerateConsoleCtrlEvent(CTRL_C_EVENT, pid) } != 0 {
                 Ok(())
+            } else {
+                Err(io::Error::last_os_error())
             }
+        } else {
+            Err(io::Error::new(io::ErrorKind::Other, "Process has no pid"))
         }
     }
 }
@@ -40,24 +53,32 @@ impl<T> Interruptable for T where T: InterruptablePid {}
 
 /// Trait for getting the pid of a child process
 pub trait InterruptablePid {
-    fn pid(&self) -> u32;
+    /// Get the pid of the child process
+    fn pid(&self) -> Option<u32>;
 }
 
 impl InterruptablePid for Child {
-    fn pid(&self) -> u32 {
+    fn pid(&self) -> Option<u32> {
+        Some(self.id())
+    }
+}
+
+#[cfg(feature = "tokio")]
+impl InterruptablePid for tokio::process::Child {
+    fn pid(&self) -> Option<u32> {
         self.id()
     }
 }
 
 /// Create a new interruptable command
 #[cfg(unix)]
-pub fn new_interruptable_command<S: AsRef<OsStr>>(program: S) -> Command {
+pub fn new_command<S: AsRef<OsStr>>(program: S) -> Command {
     Command::new(program)
 }
 
 /// Create a new interruptable command
 #[cfg(windows)]
-pub fn new_interruptable_command<S: AsRef<OsStr>>(program: S) -> Command {
+pub fn new_command<S: AsRef<OsStr>>(program: S) -> Command {
     use std::os::windows::process::CommandExt as _;
 
     let mut command = Command::new(program);
@@ -65,13 +86,27 @@ pub fn new_interruptable_command<S: AsRef<OsStr>>(program: S) -> Command {
     command
 }
 
+/// Create a new interruptable tokio command
+#[cfg(all(feature = "tokio", target_family = "windows"))]
+pub fn new_tokio_command<S: AsRef<OsStr>>(program: S) -> tokio::process::Command {
+    let mut command = tokio::process::Command::new(program);
+    command.creation_flags(CREATE_NEW_PROCESS_GROUP);
+    command
+}
+
+/// Create a new interruptable tokio command
+#[cfg(all(feature = "tokio", target_family = "unix"))]
+pub fn new_tokio_command<S: AsRef<OsStr>>(program: S) -> tokio::process::Command {
+    tokio::process::Command::new(program)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_new_interruptable_command() {
-        let mut command = new_interruptable_command("ping");
+    fn test_interruptable_command() {
+        let mut command = new_command("ping");
         #[cfg(windows)]
         command.arg("-t");
         command.arg("127.0.0.1");
@@ -79,5 +114,18 @@ mod tests {
         let mut child = command.spawn().unwrap();
         child.send_ctrl_c().unwrap();
         child.wait().unwrap();
+    }
+
+    #[cfg(feature = "tokio")]
+    #[tokio::test]
+    async fn test_tokio_interruptable_command() {
+        let mut command = new_tokio_command("ping");
+        #[cfg(windows)]
+        command.arg("-t");
+        command.arg("127.0.0.1");
+
+        let mut child = command.spawn().unwrap();
+        child.send_ctrl_c().unwrap();
+        child.wait().await.unwrap();
     }
 }
